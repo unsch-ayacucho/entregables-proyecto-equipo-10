82b43602d5723f705c0a14f39b291472d0db3b9b
Gedy Palomino <gedy.palomino@gmail.com>
2019-05-30 23:04:10 -0500

    Initial commit with files
    A quick start

---
+++ src/main/java/app/IndexController.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
